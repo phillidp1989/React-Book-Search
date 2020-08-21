b6559829e0fe94217a7af0b1615f78b71e59dd31
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-21 21:57:13 +0100

    Authentication middleware

---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Check if user is authenticated. If they are not, reroute to login page
+  authCheck: (req, res, next) => {
+    if (req.user) {
+      return next();
+    }
+    res.redirect('http://localhost:3000/');
+  },
+  // Check if user is not authenticated. If they are not a guest, reroute to member screen
+  guestCheck: (req, res, next) => {
+    if (!req.user) {
+      return next();
+    }
+    res.redirect('http://localhost:3000/');
+  },
+};
