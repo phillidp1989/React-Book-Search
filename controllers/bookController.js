fe751268f07c7c4280da95f4fc02764247379ff1
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-24 00:02:59 +0100

    Small amendments to controller methods

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,7 +3,8 @@ const Book = require('../models/Book');
 module.exports = {
   findAll: async (req, res) => {
     try {
-      const results = await Book.find({ userId: req.userId }).sort({ date: -1 });
+      console.log(req.body);
+      const results = await Book.find({ userId: req.body.providerId }).sort({ date: -1 });
       res.json(results);  
     } catch (err) {      
       console.log("ERROR - bookController.js - findAll", err);
@@ -12,6 +13,7 @@ module.exports = {
   },
   saveBook: async (req, res) => {
     try {
+      console.log(req.body);
       const result = await Book.create(req.body);
       res.json(result); 
     } catch (err) {
@@ -21,6 +23,7 @@ module.exports = {
   },
   deleteBook: async (req, res) => {
     try {
+      console.log(req.params);
       const result = await Book.remove({_id: req.params.id});
       res.status(200).json(result);
     } catch (err) {
