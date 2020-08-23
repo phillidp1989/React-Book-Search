dee9d833491c98b748272a27489e55308adbaca8
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-23 23:59:01 +0100

    Navbar component

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css';
+
+function Navbar() {
+    return (
+        <div className="navbar-fixed">
+      <nav>
+        <div className="nav-wrapper indigo darken-3">
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger"
+            ><i className="material-icons">menu</i></a>
+          <ul className="right hide-on-med-and-down">
+            <li><a href="/search">Search</a></li>
+            <li><a href="/savedbooks">Saved</a></li>
+            <li><a href="http://localhost:3001/logout"><i className="fas fa-sign-out-alt"></i></a></li>              
+            
+          </ul>
+        </div>
+      </nav>
+    </div>
+    )
+}
+
+export default Navbar;
