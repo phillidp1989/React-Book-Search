1be15d6fdd8a8a8eae1209347cf50b9c0af040e4
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-24 00:02:24 +0100

    Created methods to hit backend api routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,17 +2,33 @@ import axios from 'axios';
 
 export default {
   // Gets all books
-  getBooks: async () => {
+  getBooks: async (query) => {
     try {
-      return await axios.get("/api/books");      
+      return await axios.request({
+        method: "POST",
+        url: "http://localhost:3001/books",
+        data: query
+      });      
     } catch (err) {
-      console.error('ERROR - API.js - getBooks', err);
+      console.error('ERROR - src/API.js - getBooks', err);
+    }
+  },
+  getMyBooks: async (userId) => {
+    try {
+      return await axios.request({
+        method: "POST",
+        url: "http://localhost:3001/api/savedbooks",
+        data: userId
+      });      
+    } catch (err) {
+      console.error('ERROR - src/API.js - getBooks', err);
     }
   },  
   // Delete book using req.params.id
   deleteBook: async id => {
     try {
-      return await axios.delete("/api/books/" + id);      
+      console.log(id);
+      return await axios.delete("http://localhost:3001/api/books/" + id._id);      
     } catch (err) {
       console.error('ERROR - API.js - deleteBook', err);
     }
@@ -20,7 +36,7 @@ export default {
   // Saves a book to the database
   saveBook: async bookData => {
     try {
-      return await axios.post("/api/books", bookData);      
+      return await axios.post("http://localhost:3001/api/books", bookData);      
     } catch (err) {
       console.error('ERROR - API.js - saveBook', err);
     }
