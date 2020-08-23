cf393cdfab66afe9dbabc2327e214fd200ed211d
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-24 00:04:04 +0100

    Changed to post request

--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const getBooks = require('../utils/API');
+const { authCheck } = require("../middleware/auth");
 
-router.get('/books', async (req, res) => {
+router.post('/books', async (req, res) => {
   try {
     const results = await getBooks(req.body)
     res.status(200).json(results)
