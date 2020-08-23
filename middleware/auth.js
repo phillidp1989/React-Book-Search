04686f8db5598082f225ee7f995f6ee25a7fafab
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-24 00:03:11 +0100

    Amended redirects

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,15 +2,16 @@ module.exports = {
   // Check if user is authenticated
   authCheck: (req, res, next) => {
     if (req.user) {
+      console.log('auth check' + req.user);
       return next();
     }
-    res.redirect('http://localhost:3000/');
+    res.redirect('http://localhost:3000/login');
   },
   // Check if user is not authenticated
   guestCheck: (req, res, next) => {
     if (!req.user) {
       return next();
     }
-    res.redirect('http://localhost:3000/');
+    res.redirect('http://localhost:3000/search');
   },
 };
