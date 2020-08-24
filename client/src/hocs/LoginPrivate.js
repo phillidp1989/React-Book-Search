b27ce27aecc1862b85c1d4cc07c53f4854247c77
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-24 19:46:22 +0100

    Added %PUBLIC_URL% to private routes

--- client/src/hocs/LoginPrivate.js
+++ client/src/hocs/LoginPrivate.js
@@ -7,8 +7,10 @@ function LoginPrivate({ component: Component, ...rest }) {
   return (
     <Route {...rest} render={props => {
       if(user && isLoaded) {
-        return <Redirect to={{ pathname: '/search', state : {from: props.location} }}/>
+        console.log('redirecting to /search');
+        return <Redirect to={{ pathname: '%PUBLIC_URL%/search', state : {from: props.location} }}/>
       }
+      console.log('rendering original component');
       return <Component {...props}/>
     }}/>
   )
--- client/src/hocs/PrivateRoute.js
+++ client/src/hocs/PrivateRoute.js
@@ -7,8 +7,10 @@ function PrivateRoute({ component: Component, ...rest }) {
   return (
     <Route {...rest} render={props => {
       if(!user && isLoaded) {
-        return <Redirect to={{ pathname: '/login', state : {from: props.location} }}/>
+        console.log('redirecting to /login');
+        return <Redirect to={{ pathname: '%PUBLIC_URL%/login', state : {from: props.location} }}/>
       }
+      console.log('rendering original component');
       return <Component {...props}/>
     }}/>
   )
