a1782ee99047fdbf0ec2219215cf6ab4d7a8a64f
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-24 19:52:19 +0100

    Removed PUBLIC_URL from private route

--- client/src/hocs/LoginPrivate.js
+++ client/src/hocs/LoginPrivate.js
@@ -8,9 +8,8 @@ function LoginPrivate({ component: Component, ...rest }) {
     <Route {...rest} render={props => {
       if(user && isLoaded) {
         console.log('redirecting to /search');
-        return <Redirect to={{ pathname: '%PUBLIC_URL%/search', state : {from: props.location} }}/>
+        return <Redirect to={{ pathname: '/search', state : {from: props.location} }}/>
       }
-      console.log('rendering original component');
       return <Component {...props}/>
     }}/>
   )
--- client/src/hocs/PrivateRoute.js
+++ client/src/hocs/PrivateRoute.js
@@ -8,9 +8,8 @@ function PrivateRoute({ component: Component, ...rest }) {
     <Route {...rest} render={props => {
       if(!user && isLoaded) {
         console.log('redirecting to /login');
-        return <Redirect to={{ pathname: '%PUBLIC_URL%/login', state : {from: props.location} }}/>
-      }
-      console.log('rendering original component');
+        return <Redirect to={{ pathname: '/login', state : {from: props.location} }}/>
+      }      
       return <Component {...props}/>
     }}/>
   )
